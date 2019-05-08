e133a18e73660f25e366a6a96224c9f4e7025b23
Alix Leon <alixlm19@hotmail.com>
2019-05-08 10:46:27 -0400

    Update graphics.js
    Added the backColor function, which takes a color as an argument and changes to context canvas' backgroundColor style property

--- graphics.js
+++ graphics.js
@@ -6,3 +6,21 @@ function createCanvas(width, height) {
     document.body.appendChild(canvas);
     return canvas;
 }
+
+function backColor() {
+	c = arguments; //color arguments
+	
+	if(c.length === 1) {								//Case 1: Only 1 argument was given 
+		if(typeof(c[0]) === "number") {					//Case 1a: color
+			console.log(`a${c[0]}`);
+			context.style.backgroundColor = `${colorModel}(${c[0]}, ${c[0]}, ${c[0]})`;
+		} else if (typeof(c[0]) === "string") {
+			context.style.backgroundColor = c[0];
+		}
+	} else if(c.length === 3) {
+		context.style.backgroundColor = `rgb(${c[0]}, ${c[1]}, ${c[2]})`;
+	} else if(c.length === 4) {
+		context.style.backgroundColor = `rgba(${c[0]}, ${c[1]}, ${c[2]}, ${c[3]})`;
+	}
+	return arguments;
+}
